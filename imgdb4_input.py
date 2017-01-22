beca1457c56872d7a43334913807a76049965f73
Oliver Meynberg <oliver.meynberg@dlr.de>
2017-01-22 16:15:19 +0100

    Training logs of previous no-augmentation run saved to imgdb_train_no_augm. In this run, switched on data augmentation. Changed learning rate to 0.005 which is half of the one before.

--- imgdb4_input.py
+++ imgdb4_input.py
@@ -170,19 +170,17 @@ def distorted_inputs(data_dir, batch_size):
   # distortions applied to the image.
 
   # Randomly crop a [height, width] section of the image.
-  #distorted_image = tf.random_crop(reshaped_image, [height, width, 3])
+  distorted_image = tf.random_crop(reshaped_image, [height, width, 3])
 
   # Randomly flip the image horizontally.
-  #distorted_image = tf.image.random_flip_left_right(distorted_image)
+  distorted_image = tf.image.random_flip_left_right(distorted_image)
 
   # Because these operations are not commutative, consider randomizing
   # the order their operation.
-  #distorted_image = tf.image.random_brightness(distorted_image,
-  #                                             max_delta=63)
-  #distorted_image = tf.image.random_contrast(distorted_image,
-  #                                           lower=0.2, upper=1.8)
-
-  distorted_image = reshaped_image
+  distorted_image = tf.image.random_brightness(distorted_image,
+                                               max_delta=63)
+  distorted_image = tf.image.random_contrast(distorted_image,
+                                             lower=0.2, upper=1.8)
 
   # Subtract off the mean and divide by the variance of the pixels.
   float_image = tf.image.per_image_whitening(distorted_image)
