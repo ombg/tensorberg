d10b2775afcbef51b85b53ca83f60132b2d9b506
Oliver M <olliwaa@gmail.com>
2018-10-16 07:47:23 -0700

    Removing class base_train

--- trainers/default_trainer.py
+++ trainers/default_trainer.py
@@ -1,15 +1,51 @@
-#from data_loader.cifar_imgdb import ImgdbLoader
-from base.base_train import BaseTrain
+import tensorflow as tf
+
 from tqdm import tqdm
 import numpy as np
-from utils.logger import DefinedSummarizer
+#from utils.logger import DefinedSummarizer
 
-class Trainer(BaseTrain):
+class Trainer:
     def __init__(self, sess, model, data, config):
-        super(Trainer, self).__init__(sess, model, config, data_loader=data)
+        """
+        Constructing the trainer
+        :param sess: TF.Session() instance
+        :param model: The model instance
+        :param config: config namespace which will contain all the configurations you have specified in the json
+        :param logger: logger class which will summarize and write the values to the tensorboard
+        :param data: The data loader if specified. 
+        """
+        # Assign all class attributes
+        self.model = model
+        #self.logger = logger
+        self.config = config
+        self.sess = sess
+        if data is not None:
+            self.data = data
+
+        # Initialize all variables of the graph
+        self.init = tf.global_variables_initializer()
+        self.sess.run(self.init)
+
+    def train(self):
+        """
+        This is the main loop of training
+        Looping on the epochs
+        :return:
+        """
+        for cur_epoch in range(self.model.cur_epoch_tensor.eval(self.sess), self.config.num_epochs + 1, 1):
+            self.train_epoch()
+            self.sess.run(self.model.increment_cur_epoch_tensor)
 
     def train_epoch(self):
-        self.data_loader.initialize(self.sess)
+        """
+        implement the logic of epoch:
+        -loop over the number of iterations in the config and call the train step
+        -add any summaries you want using the summary
+
+        :param epoch: take the number of epoch if you are interested
+        :return:
+        """
+        self.data.initialize(self.sess)
         loop = tqdm(range(self.config.num_iter_per_epoch))
         losses = []
         accs = []
@@ -31,5 +67,11 @@ class Trainer(BaseTrain):
         self.model.save(self.sess)
 
     def train_step(self):
+        """
+        implement the logic of the train step
+
+        - run the tensorflow session
+        :return: any metrics you need to summarize
+        """
         _, loss, acc = self.sess.run([self.model.train_step, self.model.loss, self.model.accuracy])
         return loss, acc
