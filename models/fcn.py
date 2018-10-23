777d2c170529526a241188de8ce1f4889a204dcb
Oliver M <olliwaa@gmail.com>
2018-10-23 13:51:06 -0700

    Training on bottlenecks works.

--- models/fcn.py
+++ models/fcn.py
@@ -44,36 +44,36 @@ class FullyConnectedNet:
         """
 
         # fc3
-#        with tf.variable_scope('fc3') as scope:
-#            fc3w = tf.get_variable(name='weights',
-#                                   shape=[4096, 5],
-#                                   initializer=tf.glorot_uniform_initializer(),
-#                                   regularizer=tf.nn.l2_loss,
-#                                   trainable=True)
-#            fc3b = tf.get_variable(name='biases',
-#                                   shape=[5],
-#                                   initializer=tf.zeros_initializer(),
-#                                   trainable=True)
-#            self.fc3l = tf.nn.bias_add(tf.matmul(self.bottlenecks, fc3w), fc3b)
-#            self.parameters += [fc3w, fc3b]
-#
-#        # Compute data loss and regularization loss
-#        with tf.name_scope('loss'):
-#            cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
-#                labels=self.labels,
-#                logits=self.fc3l)
-#            data_loss = tf.reduce_mean(cross_entropy)
-#            reg_loss = tf.losses.get_regularization_loss()
-#            self.loss = tf.add(data_loss, reg_loss, name='data_and_reg_loss')
-#            correct_prediction = tf.equal(tf.argmax(self.fc3l, 1), tf.argmax(self.labels, 1))
-#            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-#            tf.summary.scalar('total_loss', self.loss)
-#            tf.summary.scalar('accuracy', self.accuracy)
-#
-#        with tf.name_scope('train_step'):
-#            global_step=tf.train.get_or_create_global_step()
-#            self.optimizer = tf.train.AdamOptimizer(self.config.learning_rate)
-#            self.train_step = self.optimizer.minimize(self.loss, global_step=global_step)
-#
-#        with tf.name_scope('test'):
-#            self.softmax = tf.nn.softmax(self.fc3l)
+        with tf.variable_scope('fc3') as scope:
+            fc3w = tf.get_variable(name='weights',
+                                   shape=[4096, 5],
+                                   initializer=tf.glorot_uniform_initializer(),
+                                   regularizer=tf.nn.l2_loss,
+                                   trainable=True)
+            fc3b = tf.get_variable(name='biases',
+                                   shape=[5],
+                                   initializer=tf.zeros_initializer(),
+                                   trainable=True)
+            self.fc3l = tf.nn.bias_add(tf.matmul(self.bottlenecks, fc3w), fc3b)
+            self.parameters += [fc3w, fc3b]
+
+        # Compute data loss and regularization loss
+        with tf.name_scope('loss'):
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
+                labels=self.labels,
+                logits=self.fc3l)
+            data_loss = tf.reduce_mean(cross_entropy)
+            reg_loss = tf.losses.get_regularization_loss()
+            self.loss = tf.add(data_loss, reg_loss, name='data_and_reg_loss')
+            correct_prediction = tf.equal(tf.argmax(self.fc3l, 1), tf.argmax(self.labels, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            tf.summary.scalar('total_loss', self.loss)
+            tf.summary.scalar('accuracy', self.accuracy)
+
+        with tf.name_scope('train_step'):
+            global_step=tf.train.get_or_create_global_step()
+            self.optimizer = tf.train.AdamOptimizer(self.config.learning_rate)
+            self.train_step = self.optimizer.minimize(self.loss, global_step=global_step)
+
+        with tf.name_scope('test'):
+            self.softmax = tf.nn.softmax(self.fc3l)
--- trainers/default_trainer.py
+++ trainers/default_trainer.py
@@ -79,7 +79,7 @@ class Trainer:
             val_writer.add_summary(summary_val, global_step=global_step_vl)
             val_writer.flush()
 
-        save_path = saver.save(self.sess, self.config.checkpoint_dir + '/run_' + str(run_id))
+        save_path = saver.save(self.sess, self.config.checkpoint_dir + 'run_' + str(run_id))
         print('Model checkpoint saved to %s' % save_path)
     
         train_writer.close()
