094b7c59c53d136281f7b28b59e772e46bcb3bc9
Oliver M <olliwaa@gmail.com>
2019-01-23 09:20:44 -0800

    Added toy net to test decorators.

--- train_classifier.py
+++ train_classifier.py
@@ -3,7 +3,7 @@ import pprint
 
 import tensorflow as tf
 
-from models.classification import FullyConnectedNet
+from models.classification import ToyModel
 from trainers.default_trainer import ClassificationTrainer
 
 from utils.datahandler import TFRecordDatasetLoader
@@ -12,40 +12,36 @@ from utils.general_utils import get_args
 from utils.data_utils import parse_txt
 
 def main():
-    try:
-        # capture the config path from the run arguments
-        # then process the json configration file and print it
-        args = get_args()
-        config = process_config(args.config)
-    
-        # create tensorflow session
-        sess = tf.Session()
-    
-        # Loads data into a tf.dataset
-        data_loader = TFRecordDatasetLoader(config)
-    
-        data_loader.load_datasets(do_shuffle=True,
-                                  train_repetitions=-1)
-        # create instance of the model 
-        model = FullyConnectedNet(config, data_loader=data_loader)
-    
-        # TODO There is a fancy way to get rid of this using decorators:
-        # https://danijar.com/structuring-your-tensorflow-models/
-        model.build_graph()
-    
-        # Trainer loops over the data using the model
-        trainer = ClassificationTrainer(sess,
-                                        model,
-                                        config,
-                                        data_loader=data_loader)
-    
-        trainer.train()
-        #trainer.test(config.checkpoint_to_restore_path)
-        trainer.test()
-        tf.logging.info('==== Configuration ====')
-        tf.logging.info(pprint.pprint(config))
-    except Exception as err:
-        tf.logging.error('Unhandled exception:\n{}'.format(err.args))
+    # capture the config path from the run arguments
+    # then process the json configration file and print it
+    args = get_args()
+    config = process_config(args.config)
+
+    # create tensorflow session
+    sess = tf.Session()
+
+    # Loads data into a tf.dataset
+    data_loader = TFRecordDatasetLoader(config)
+
+    data_loader.load_datasets(do_shuffle=True,
+                              train_repetitions=-1)
+    # create instance of the model 
+    model = ToyModel(data_loader=data_loader)
+
+    # TODO There is a fancy way to get rid of this using decorators:
+    # https://danijar.com/structuring-your-tensorflow-models/
+
+    # Trainer loops over the data using the model
+    trainer = ClassificationTrainer(sess,
+                                    model,
+                                    config,
+                                    data_loader=data_loader)
+
+    trainer.train()
+    #trainer.test(config.checkpoint_to_restore_path)
+    trainer.test()
+    tf.logging.info('==== Configuration ====')
+    tf.logging.info(pprint.pprint(config))
 
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
