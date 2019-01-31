e84ee13909f7139d8495555e2610863ae23a2b34
Oliver M <olliwaa@gmail.com>
2019-01-31 10:42:08 -0800

    TF datasets train and test. Switching in train loop

--- sandbox_compare_tensorf_w_numpy.py
+++ sandbox_compare_tensorf_w_numpy.py
@@ -11,19 +11,41 @@ from ompy import plotml, ml
 def main():
 
     d =  np.load('/tmp/spiral_data.npy')
-    features = d.item().get('features')
-    labels = d.item().get('labels')
-    labels_scalar = d.item().get('labels_scalar')
-    num_samples, num_classes = labels.shape
-    num_features=features.shape[1]
-    dset_train = tf.data.Dataset.from_tensor_slices((features, labels))
-    #dset_train = dset_train.shuffle(num_samples)
-    dset_train = dset_train.repeat(-1)
-    dset_train = dset_train.batch(features.shape[0]) #Use whole X, as in ml.train_twolayernet()
-    iter_train = dset_train.make_one_shot_iterator()
-    next_element = iter_train.get_next()
+    train_x = d.item().get('features')
+    train_y = d.item().get('labels')
+    train_y_scalar = d.item().get('labels_scalar')
+    train_x = train_x.astype('float32')
+    train_y = train_y.astype('float32')
+
+    d =  np.load('/tmp/spiral_testdata.npy')
+    test_x = d.item().get('features')
+    test_y = d.item().get('labels')
+    test_y_scalar = d.item().get('labels_scalar')
+    test_x = test_x.astype('float32')
+    test_y = test_y.astype('float32')
+
+    # Create datasets
+    trainset = tf.data.Dataset.from_tensor_slices((train_x,train_y))
+    trainset = trainset.repeat(-1)
+    trainset = trainset.batch(900)
+    testset = tf.data.Dataset.from_tensor_slices((test_x,test_y))
+    testset = testset.repeat(-1)
+    testset = testset.batch(300) # Set to size of testset
+    handle = tf.placeholder(tf.string, shape=[])
+    # Create iterator
+    iterator = tf.data.Iterator.from_string_handle(handle, trainset.output_types,
+                                                           trainset.output_shapes)
+    next_element = iterator.get_next()
+
+    train_iter = trainset.make_one_shot_iterator()
+    test_iter = testset.make_one_shot_iterator()
+
     data = next_element[0]
     label = next_element[1]
+
+    num_samples, num_classes = train_y.shape
+    num_features= train_x.shape[1]
+
     # create instance of the model 
     hidden1, fc1w, fc1b = layers.fc(data, num_features, 100, name='hidden1',log_weights=False)
     logits, fc2w, fc2b = layers.fc(hidden1, 100, num_classes, name='logits', relu=False, log_weights=False)
@@ -43,32 +65,36 @@ def main():
     with tf.Session() as sess:
         #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
         sess.run(tf.global_variables_initializer())
+        train_handle = sess.run(train_iter.string_handle())
+        test_handle = sess.run(test_iter.string_handle())
         global_step=tf.train.get_or_create_global_step()
-        for _ in range(10000):
-            #sess.run(next_element)
+        for _ in range(1000):
             fetches = [ error,
                         loss,
                         optimize,
                         global_step]
-            error_vl, loss_vl, _, global_step_vl = sess.run(fetches)
-            if global_step_vl % 1000 == 0:
+            error_vl, loss_vl, _, global_step_vl = sess.run(fetches, feed_dict={handle: train_handle})
+            if global_step_vl % 50 == 0:
                 print('{}#: Training error {:6.2f}% - Training set loss: {:6.2f}'.format(
+                      global_step_vl, 100 * error_vl, loss_vl ))
+                error_vl, loss_vl, global_step_vl = sess.run([error,loss, global_step], feed_dict={handle: test_handle})
+                print('{}#: Test error {:6.2f}% - Test set loss: {:6.2f}'.format(
                       global_step_vl, 100 * error_vl, loss_vl ))
 
         # Plot final classifier boundaries
         fetches = [fc1w, fc1b,fc2w,fc2b]
-        W1, b1, W2, b2 = sess.run(fetches)
-        plotml.plot2Dclassifier(W1, b1, W2, b2, features, labels_scalar)
-        plotml.plot2layerNetClassifier(features, W1, W2, b1, b2, labels_scalar)
+        W1, b1, W2, b2 = sess.run(fetches, feed_dict={handle: train_handle})
+        plotml.plot2Dclassifier(W1, b1, W2, b2, test_x, test_y_scalar)
+        #plotml.plot2layerNetClassifier(features, W1, W2, b1, b2, labels_scalar)
 
     # Alternatively, run without TF and plot for comparison with TF version
-    w = ml.train_twolayernet(features, labels_scalar)
-    plotml.plot2Dclassifier(w['W1'], w['b1'], w['W2'], w['b2'],
-                            features, labels_scalar,
-                            save_to='/tmp/toy_spiral_w_boundaries.png')
-    plotml.plot2layerNetClassifier(features, w['W1'], w['W2'], w['b1'], w['b2'],
-                                   labels_scalar,
-                                   save_to='/tmp/toy_spiral_w_boundaries_B.png')
+    #w = ml.train_twolayernet(features, labels_scalar)
+    #plotml.plot2Dclassifier(w['W1'], w['b1'], w['W2'], w['b2'],
+    #                        features, labels_scalar,
+    #                        save_to='/tmp/toy_spiral_w_boundaries.png')
+    #plotml.plot2layerNetClassifier(features, w['W1'], w['W2'], w['b1'], w['b2'],
+    #                               labels_scalar,
+    #                               save_to='/tmp/toy_spiral_w_boundaries_B.png')
 
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
